2b58390cb61b9aee7e634101fe2183e446cc1bed
carlosmartins77 <carlosmatrins@gmail.com>
2023-07-01 11:57:52 +0100

    Removed debug prints, add printed result

--- Sequencial/JobShop.c
+++ Sequencial/JobShop.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct machine
+typedef struct
 {
     int machineId;
     bool isWorking;
     int endTime;
 } Machine;
 
-typedef struct operation
+typedef struct
 {
     int jobId;
     int machineId;
     int duration;
 } Operation;
 
-typedef struct job
+typedef struct
 {
     Operation operations[100];
 } Job;
+
 
 
 int main(int argc, char **argv)
@@ -27,11 +28,8 @@ int main(int argc, char **argv)
     int nJobs;
     int nMachines;
 
-    inputFile = fopen("jobshop1.txt", "r");
+    inputFile = fopen(argv[1], "r");
     fscanf(inputFile, "%d %d", &nJobs, &nMachines);
-
-    printf("1: %d\n", nJobs);
-    printf("2: %d\n", nMachines);
 
     Job jobs[100];
     Machine machines[100];
@@ -46,7 +44,7 @@ int main(int argc, char **argv)
     for (int i = 0; i < nJobs; i++) {
         for (int m = 0; m < nMachines; m++) {
             fscanf(inputFile, "%d  %d", &jobs[i].operations[m].machineId, &jobs[i].operations[m].duration);
-            printf("\nMachine:%d Duration:%d", jobs[i].operations[m].machineId, jobs[i].operations[m].duration);
+            //printf("\nMachine:%d Duration:%d", jobs[i].operations[m].machineId, jobs[i].operations[m].duration);
             jobs[i].operations[m].jobId = i;
         }
     }
@@ -55,34 +53,27 @@ int main(int argc, char **argv)
     int endTime[100][100];     // Array para guardar end time
     int maxEndTime = 0; // Guardar o ultimo tempo
     
-    for (int m = 0; m < nMachines; m++) // percore as operaçoes
+    for (int m = 0; m < nMachines; m++) // percore as maquinas (Linha de operacoes)
     {
         // Percorrer as linhas
-        for (int j = 0; j < nJobs; j++) // Traverse the jobs
-        {
-            
+        for (int j = 0; j < nJobs; j++) // Percorre os jobs (combinaçao maquina, operacao)
+        {   
             if (machines[jobs[j].operations[m].machineId].isWorking == true) 
             {
                 // o tempo inicial da operacao 0 do job 1 é igual ao tempo da operacao 0 do job 0 
                 initialTime[j][m] = machines[jobs[j].operations[m].machineId].endTime; // Agora vai aceder à maquina que fez a operaçao 1 do job 0, que neste caso foi a maquina 0 que teve o end time definido na iteraçao 0,0      
                 endTime[j][m] = initialTime[j][m] + jobs[j].operations[m].duration;
 
-                // Block the machine
-                machines[jobs[j].operations[m].machineId].isWorking = true;
+                machines[jobs[j].operations[m].machineId].isWorking = true; //ocupada
                 machines[jobs[j].operations[m].machineId].endTime = endTime[j][m];
-                
-                printf("\naqui aqui %d", machines[jobs[j].operations[m].machineId].endTime);
             }
             else // se a maquina responsavel pela operaçao 0 do job 0 esta livre..
             {
                 initialTime[j][m] = maxEndTime;        // a opercao 0 começa aos 0                                        
                 endTime[j][m] = initialTime[j][m] + jobs[j].operations[m].duration; // A operaçao 0 do job 0 termina aos 0 + o tempo de duraçao do job 0 da maquina 0
 
-                // Mark machine as occupied
                 machines[jobs[j].operations[m].machineId].isWorking = true; // a maquina 0 passou a estar ocupada
-                machines[jobs[j].operations[m].machineId].endTime = endTime[j][m]; //O tempo de término da operaçao 0 realizada na maquina 0 é igual ao endtime definido anteriormente
-                
-                printf("\naqui %d", machines[jobs[j].operations[m].machineId].endTime);
+                machines[jobs[j].operations[m].machineId].endTime = endTime[j][m]; //O tempo de término da operaçao 0 realizada na maquina 0 é igual ao endtime definido anteriormente   
             }
         }
 
@@ -104,5 +95,15 @@ int main(int argc, char **argv)
         }
     
     }
+
+    // Print do resultado
+    for (int j = 0; j < nJobs; j++) {
+        for (int m = 0; m < nMachines; m++) {
+            printf("Job %d, Machine %d, Start Time: %d, End Time: %d\n", j, jobs[j].operations[m].machineId, initialTime[j][m], endTime[j][m]);
+        }
+    }
+
+
+
     return 0;
 }
